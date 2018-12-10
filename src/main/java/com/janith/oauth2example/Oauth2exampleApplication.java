6d2c35497f86876d03c0ea0eb18b083af54c20b4
Janith Thilakarathna <chamarajc@gmail.com>
2018-12-11 01:43:50 +0530

    Initial Commit

---
+++ src/main/java/com/janith/oauth2example/Oauth2exampleApplication.java
@@ -0,0 +1,12 @@
+package com.janith.oauth2example.oauth2example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2exampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2exampleApplication.class, args);
+    }
+}
