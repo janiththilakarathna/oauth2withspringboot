9c26c64ae361f167140d1a8635dd9af8858fd54e
Janith Thilakarathna <chamarajc@gmail.com>
2018-12-13 16:35:11 +0530

    configured h2

--- src/main/java/com/janith/oauth2example/Oauth2exampleApplication.java
+++ src/main/java/com/janith/oauth2example/Oauth2exampleApplication.java
@@ -1,4 +1,4 @@
-package com.janith.oauth2example.oauth2example;
+package com.janith.oauth2example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table student
+(
+  id           number primary KEY,
+  name         VARCHAR(60) not null,
+  registration VARCHAR(60) not null,
+  primary key (id)
+);
